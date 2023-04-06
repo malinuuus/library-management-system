180392bf953a8d668a498c166062076901bc6bb4
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-06 20:38:51 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Title</title>
+</head>
+<body>
+    <nav class="main-nav">
+        <h1>Library Management System</h1>
+        <div>
+            <a href="">log out</a>
+        </div>
+    </nav>
+    <div class="side-bar">
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Books</a></li>
+            <li><a href="">Authors</a></li>
+            <li><a href="">Categories</a></li>
+        </ul>
+    </div>
+    <div class="content">
+        <!-- here goes the dynamic content -->
+    </div>
+</body>
+</html>
