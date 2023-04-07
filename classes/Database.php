5a2a1c02d9a16b4170e4bb1b75f6593382f2b5fc
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-08 00:45:13 +0200

    Add protection from sql injection

--- classes/Database.php
+++ classes/Database.php
@@ -2,16 +2,39 @@
 
 class Database {
     private $conn;
+    private $statement;
 
     public function __construct($database, $hostname = "localhost", $username = "root", $password = "") {
         $this->conn = new mysqli($hostname, $username, $password, $database);
     }
 
-    public function getResult($query) {
-        return $this->conn->query($query);
+    public function getResult($query, $parameters = array()) {
+        $this->statement = $this->conn->prepare($query);
+
+        if (sizeof($parameters) != 0) {
+            $types = "";
+
+            foreach ($parameters as $value) {
+                switch (gettype($value)) {
+                    case "boolean":
+                    case "integer": $types .= "i"; break;
+                    case "double": $types .= "d"; break;
+                    case "string": $types .= "s"; break;
+                    default: $types .= "b"; break;
+                }
+            }
+
+            $this->statement->bind_param($types, ...$parameters);
+        }
+
+        $this->statement->execute();
+        return $this->statement->get_result();
     }
 
     public function close() {
+        if (isset($this->statement)) {
+            $this->statement->close();
+        }
         $this->conn->close();
     }
 }
