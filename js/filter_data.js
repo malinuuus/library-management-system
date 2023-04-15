8c1ba3ec8ed27ee013852f79588c7fdf1f416a41
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-16 00:47:38 +0200

    Make function to filter rows

--- js/filter_data.js
+++ js/filter_data.js
@@ -1,20 +1,28 @@
 const searchBar = document.getElementById('search-bar');
 const booksArray = document.querySelectorAll('.book-row');
-const filterColumns = ['title', 'author', 'category'];
+const authorsArray = document.querySelectorAll('.author-info');
 
-searchBar.addEventListener('keyup', () => {
-    booksArray.forEach(book => {
-        book.style.display = "none";
-        const tdArray = book.getElementsByTagName('td');
+function filterRows(array, tagName, filterColumns) {
+    array.forEach(item => {
+        item.style.display = "none";
+        const tdArray = item.getElementsByTagName(tagName);
 
         for (let i = 0; i < tdArray.length; i++) {
             if (
                 filterColumns.some(column => tdArray[i].className.includes(column)) &&
                 tdArray[i].textContent.toLowerCase().includes(searchBar.value.toLowerCase())
             ) {
-                book.style.display = "";
+                item.style.display = "";
                 break;
             }
         }
     })
+}
+
+searchBar.addEventListener('keyup', () => {
+    if (booksArray.length !== 0) {
+        filterRows(booksArray, 'td', ['title', 'author', 'category']);
+    } else if (authorsArray.length !== 0) {
+        filterRows(authorsArray, 'h4', ['name']);
+    }
 })
