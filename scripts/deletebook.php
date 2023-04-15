0220d0f190c2b7718378362444026c3344385406
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-15 22:57:33 +0200

    Delete a book cover file before deleting book data

--- scripts/deletebook.php
+++ scripts/deletebook.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
 require_once "../classes/Database.php";
+
+// delete an image if exists
 $db = new Database("library_db");
+$result = $db->getResult("SELECT cover_file_name FROM books WHERE id = ?", array($_POST["book_id"]));
+$fileName = $result->fetch_assoc()["cover_file_name"];
+
+require_once "files.php";
+deleteFile("../images/books/", $fileName);
+
 $db->getResult("DELETE FROM books WHERE id = ?", array($_POST["book_id"]));
 
 if ($db->checkAffectedRows(1)) {
