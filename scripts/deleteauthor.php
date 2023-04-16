95927dfbdff4f5f9b6716c53182f194886507b52
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-16 18:48:53 +0200

    Make function for deleting an item from the database with an image

--- scripts/deleteauthor.php
+++ scripts/deleteauthor.php
@@ -1,23 +1,3 @@
 <?php
-session_start();
-require_once "../classes/Database.php";
-
-// delete an image if exists
-$db = new Database("library_db");
-$result = $db->getResult("SELECT image_file_name FROM authors WHERE id = ?", array($_POST["author_id"]));
-$fileName = $result->fetch_assoc()["image_file_name"];
-
-require_once "files.php";
-deleteFile("../images/authors/", $fileName);
-
-$db->getResult("DELETE FROM authors WHERE id = ?", array($_POST["author_id"]));
-
-if ($db->checkAffectedRows(1)) {
-    $_SESSION["err"] = "The author has been deleted";
-} else {
-    $_SESSION["err"] = "Error occurred while deleting the author!";
-}
-
-header("location: ../index.php?page=authors");
-
-$db->close();
+require_once "delete.php";
+deleteItem($_POST["author_id"], "author", "../images/authors/", "../index.php?page=authors");
--- scripts/deletebook.php
+++ scripts/deletebook.php
@@ -1,23 +1,3 @@
 <?php
-session_start();
-require_once "../classes/Database.php";
-
-// delete an image if exists
-$db = new Database("library_db");
-$result = $db->getResult("SELECT cover_file_name FROM books WHERE id = ?", array($_POST["book_id"]));
-$fileName = $result->fetch_assoc()["cover_file_name"];
-
-require_once "files.php";
-deleteFile("../images/books/", $fileName);
-
-$db->getResult("DELETE FROM books WHERE id = ?", array($_POST["book_id"]));
-
-if ($db->checkAffectedRows(1)) {
-    $_SESSION["err"] = "The book has been deleted";
-} else {
-    $_SESSION["err"] = "Error occurred while deleting the book!";
-}
-
-header("location: ../index.php?page=books");
-
-$db->close();
+require_once "delete.php";
+deleteItem($_POST["book_id"], "book", "../images/books/", "../index.php?page=books");
