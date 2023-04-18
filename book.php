c3fac2816339b7ce76c9be29eab437003bba9ce0
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-18 20:43:36 +0200

    Change add book page to use it also for updating a book

--- book.php
+++ book.php
@@ -28,10 +28,34 @@ if (!$user["is_admin"]) {
     <title>Add a book</title>
 </head>
 <body>
+<?php
+if (isset($_SESSION["updatingBookId"])) {
+    echo $_SESSION["updatingBookId"];
+}
+?>
     <div class="wrapper">
         <h3>Add a new book to database</h3>
         <form action="scripts/addbook.php" method="post" enctype="multipart/form-data">
-            <input type="text" name="title" placeholder="Title">
+            <?php
+            $updatedTitle = "";
+            $updatedAuthorId = 0;
+            $updatedCategoryId = 0;
+            $updatedNumOfCopies = 0;
+
+            if (isset($_SESSION["updatingBookId"])) {
+                $result = $db->getResult(
+                        "SELECT b.title, a.id AS a_id, c.id AS c_id, COUNT(c2.id) AS copies FROM books b INNER JOIN authors a on b.author_id = a.id INNER JOIN categories c on b.category_id = c.id INNER JOIN copies c2 on b.id = c2.book_id WHERE b.id = ?",
+                    array($_SESSION["updatingBookId"])
+                );
+                $book = $result->fetch_assoc();
+                $updatedTitle = $book["title"];
+                $updatedAuthorId = $book["a_id"];
+                $updatedCategoryId = $book["c_id"];
+                $updatedNumOfCopies = $book["copies"];
+            }
+
+            echo "<input type='text' name='title' placeholder='Title' value='$updatedTitle'>";
+            ?>
             <label>
                 Author
                 <select name="author_id">
@@ -39,7 +63,11 @@ if (!$user["is_admin"]) {
                     $result = $db->getResult("SELECT id, first_name, last_name FROM authors");
 
                     while ($author = $result->fetch_assoc()) {
-                        echo "<option value='$author[id]'>$author[first_name] $author[last_name]</option>";
+                        if ($author["id"] === $updatedAuthorId) {
+                            echo "<option selected value='$author[id]'>$author[first_name] $author[last_name]</option>";
+                        } else {
+                            echo "<option value='$author[id]'>$author[first_name] $author[last_name]</option>";
+                        }
                     }
                     ?>
                 </select>
@@ -51,18 +79,30 @@ if (!$user["is_admin"]) {
                     $result = $db->getResult("SELECT * FROM categories");
 
                     while ($category = $result->fetch_assoc()) {
-                        echo "<option value='$category[id]'>$category[category]</option>";
+                        if ($category["id"] === $updatedCategoryId) {
+                            echo "<option selected value='$category[id]'>$category[category]</option>";
+                        } else {
+                            echo "<option value='$category[id]'>$category[category]</option>";
+                        }
                     }
 
                     $db->close();
                     ?>
                 </select>
             </label>
-            <input type="number" name="num_copies" placeholder="Number of copies">
+            <?php
+            echo "<input type='number' name='num_copies' placeholder='Number of copies' value='$updatedNumOfCopies'>";
+            ?>
             <label for="image">Image: </label>
             <input type="file" name="image" id="image" accept="image/*">
-            <button type="submit">Add</button>
             <?php
+            if (isset($_SESSION["updatingBookId"])) {
+                echo "<button type='submit'>Update</button>";
+                unset($_SESSION["updatingBookId"]);
+            } else {
+                echo "<button type='submit'>Add</button>";
+            }
+
             if (isset($_SESSION["err"])) {
                 echo "<span>$_SESSION[err]</span>";
                 unset($_SESSION["err"]);
