8eca8b51ca831f91bab03c99bd2951f24acef6b0
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-22 14:20:41 +0200

    Style the login page

--- login.php
+++ login.php
@@ -5,24 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/login.css">
     <title>Login page</title>
 </head>
 <body>
-    <div class="wrapper">
-        <h1>Library Management System</h1>
-        <p>Log in to access</p>
-        <form action="scripts/authenticate.php" method="post">
-            <input type="text" name="email" placeholder="Email">
-            <input type="password" name="password" placeholder="Password">
-            <input type="submit" value="Login">
-            <?php
-            session_start();
-            if (isset($_SESSION["err"])) {
-                echo "<span>$_SESSION[err]</span>";
-                unset($_SESSION["err"]);
-            }
-            ?>
-        </form>
+    <div class="content">
+        <div class="wrapper">
+            <h1>Library Management System</h1>
+            <p>Log in to access</p>
+            <form action="scripts/authenticate.php" method="post">
+                <input type="text" name="email" placeholder="Email">
+                <input type="password" name="password" placeholder="Password">
+                <input type="submit" value="Login">
+                <?php
+                session_start();
+                if (isset($_SESSION["err"])) {
+                    echo "<span>$_SESSION[err]</span>";
+                    unset($_SESSION["err"]);
+                }
+                ?>
+            </form>
+        </div>
     </div>
 </body>
 </html>
