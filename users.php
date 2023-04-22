e559c3ed77528d0d6acb7bab7b8ebb29dcc03918
malinuuus <maliny.brzoskwiniowe@gmail.com>
2023-04-22 13:01:58 +0200

    Add number of borrowed books to users list

--- users.php
+++ users.php
@@ -1,5 +1,12 @@
 <h3>Users list</h3>
 <table class="users-list">
+    <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Admin rights</th>
+        <th>Number of borrowed books</th>
+    </tr>
+
     <?php
     require_once "classes/Database.php";
     $db = new Database("library_db");
@@ -7,11 +14,15 @@
 
     while ($user = $result->fetch_assoc()) {
         $admin = $user["is_admin"] ? "admin" : "";
+        $booksCountResult = $db->getResult("SELECT COUNT(r.id) AS count FROM users u JOIN reservations r on u.id = r.user_id WHERE u.id = $user[id]");
+        $booksCount = $booksCountResult->fetch_assoc()["count"];
+
         echo <<< USER
             <tr class="user-info" onclick="window.location='index.php?page=userProfile&id=$user[id]'">
                 <td>$user[first_name]</td>
                 <td>$user[last_name]</td>
                 <td>$admin</td>
+                <td>$booksCount</td>
             </tr>
         USER;
     }
